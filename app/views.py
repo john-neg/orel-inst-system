4550dfea5196010cf9f5f182c2073ee43d397be3
john-neg <john_neg@mail.ru>
2021-11-01 10:00:06 +0300

    update

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/calendar')
+    def calendar():
+    return render_template('calendar.html')
---
+++ temp/test.py
@@ -0,0 +1,8 @@
+import requests
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
