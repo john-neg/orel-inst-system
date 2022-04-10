24d3603594730cafc096c2eb13289b912881f40d
john-neg <john_neg@mail.ru>
2022-04-10 13:04:07 +0300

    initial db create script added

--- scripts/start_db_create.py
+++ scripts/start_db_create.py
@@ -1,11 +1,15 @@
-from flask_login import UserMixin
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db, login
-from config import DbRoles
+from config import FlaskConfig, DbRoles
+
+app = Flask(__name__)
+app.config.from_object(FlaskConfig)
+db = SQLAlchemy(app)
 
 
-class User(UserMixin, db.Model):
+class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -25,6 +29,24 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
 
-@login.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+db.create_all()
+
+# Create base users
+
+u = User(username='admin', role=DbRoles.ROLE_ADMIN)
+u.set_password('admin')
+db.session.add(u)
+
+u = User(username='user', role=DbRoles.ROLE_USER)
+u.set_password('user')
+db.session.add(u)
+
+u = User(username='metod', role=DbRoles.ROLE_METOD)
+u.set_password('metod')
+db.session.add(u)
+
+u = User(username='bibl', role=DbRoles.ROLE_BIBL)
+u.set_password('bibl')
+db.session.add(u)
+
+db.session.commit()
