b4d2da9e765e3609f2e4ca192f40af7a943468f2
john_neg@mail.ru <Jn83Eb61!>
2022-05-12 16:55:32 +0300

    refactoring

--- app/common/classes/LessonsData.py
+++ app/common/classes/LessonsData.py
@@ -1,10 +1,3 @@
-
-# schedule_day_schedule_lessons - занятия
-# schedule_day_schedule_lessons_staff - staff_id для каждого занятия {lesson_id:staff_id}
-# load_subgroups - подгруппы
-# load_groups
-# plan_education_plans_education_forms
-# plan_education_plans
 from dataclasses import dataclass
 
 
@@ -22,8 +15,9 @@ class LessonsDataProcessor:
 
     def __post_init__(self):
         self.lessons_staff_data = self.lessons_staff_processor(self.schedule_lessons_staff)
-        self.load_groups_data = self.load_groups_processor(self.load_groups)
-        self.load_subgroups_data = self.load_groups_processor(self.load_subgroups)
+        self.load_groups_data = self.data_processor(self.load_groups)
+        self.load_subgroups_data = self.data_processor(self.load_subgroups)
+        self.load_education_plans_data =
 
     @staticmethod
     def lessons_staff_processor(lessons_staff):
@@ -41,14 +35,47 @@ class LessonsDataProcessor:
         return data
 
     @staticmethod
-    def load_groups_processor(load_groups):
-        """
-        Get group info by group_id
-        {id:[{}].
-        """
+    def data_processor(load_groups):
+        """Преобразует информацию о группах в словарь {id: 'name'}."""
         data = {}
         for group in load_groups:
-            data[group.get("id")] = group
+            data[int(group.get("id"))] = group
         return data
 
+    @staticmethod
+    def education_plans_processor(
+            plan_education_plans,
+            plan_education_plans_education_forms
+    ):
+        # cls.data_processor(self.plan_education_plans)
+        # self.data_processor(plan_education_plans_education_forms)
+        # """Преобразует информацию о группах в словарь {id: 'name'}."""
+        # data = {}
+        # for group in load_groups:
+        #     data[int(group.get("id"))] = group
+        # return data
 
+
+# 'schedule_day_schedule_lessons' - занятия
+# 'schedule_day_schedule_lessons_staff' - staff_id для каждого занятия {lesson_id:staff_id}
+# 'load_subgroups' - подгруппы
+# 'load_groups'
+# 'plan_education_plans_education_forms'
+# 'plan_education_plans'
+
+
+import requests
+from pprint import pprint
+from config import ApeksConfig as Apeks
+
+
+def db_request(table_name):
+    """DB request function without filter."""
+    params = {"token": Apeks.TOKEN, "table": table_name}
+    response = requests.get(
+        Apeks.URL + "/api/call/system-database/get", params=params
+    )
+    return response.json()["data"]
+
+
+pprint(db_request('plan_education_plans_education_forms'))
