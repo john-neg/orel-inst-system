ff2a8068ff6593047ae2d9d15b5179ea0267c3ac
john-neg <john_neg@mail.ru>
2022-05-12 18:21:46 +0300

    telegram bot requests optimization

--- app/telegram/schedule.py
+++ app/telegram/schedule.py
@@ -8,8 +8,17 @@ from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
 
 from app.common.classes.EducationStaff import EducationStaff
-from app.common.func import get_departments
-from config import FlaskConfig
+from app.common.classes.ScheduleLessonsStaff import ScheduleLessonsStaff
+from app.common.func import (
+    get_departments,
+    get_state_staff,
+    check_api_db_response,
+    api_get_db_table,
+    check_api_staff_lessons_response,
+    api_get_staff_lessons,
+    get_disciplines,
+)
+from config import FlaskConfig, ApeksConfig as Apeks
 
 
 class GetSchedule(StatesGroup):
@@ -34,6 +43,7 @@ async def schedule_start(message: types.Message):
 
 
 async def schedule_date_chosen(message: types.Message, state: FSMContext):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     cur_date = datetime.now().replace(day=1)
     available_dates = [
         cur_date.strftime("%m.%Y"),
@@ -45,9 +55,8 @@ async def schedule_date_chosen(message: types.Message, state: FSMContext):
     await state.update_data(chosen_month=int(message.text.lower().split(".")[0]))
     await state.update_data(chosen_year=int(message.text.lower().split(".")[1]))
 
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-
     departments = await get_departments()
+    await state.update_data(departments=departments)
     dept_list = [v.get("short") for v in departments.values()]
     for i in range(1, len(dept_list), 2):
         keyboard.add(dept_list[i - 1], dept_list[i])
@@ -57,7 +66,10 @@ async def schedule_date_chosen(message: types.Message, state: FSMContext):
 
 
 async def schedule_department_chosen(message: types.Message, state: FSMContext):
-    departments = await get_departments()
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    saved_data = await state.get_data()
+
+    departments = saved_data["departments"]
     dept_dict = {}
     for key, val in departments.items():
         dept_dict[val.get("short").lower()] = key
@@ -66,58 +78,67 @@ async def schedule_department_chosen(message: types.Message, state: FSMContext):
             "Пожалуйста, выберите вашу кафедру, используя клавиатуру ниже."
         )
         return
+
     department_id = dept_dict.get(message.text.lower())
     await state.update_data(chosen_department=department_id)
 
-    saved_data = await state.get_data()
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # TODO добавить запросы и передать их в класс (попробовать хранить запрос в state чтобы не перезапрашивать потом)
-    staff_list = EducationStaff(
-        saved_data["chosen_month"], saved_data["chosen_year"]
+    education_list = EducationStaff(
+        year=saved_data["chosen_year"],
+        month_start=saved_data["chosen_month"],
+        month_end=saved_data["chosen_month"],
+        state_staff=await get_state_staff(),
+        state_staff_history=await check_api_db_response(
+            await api_get_db_table(Apeks.TABLES.get("state_staff_history"))
+        ),
+        state_staff_positions=await check_api_db_response(
+            await api_get_db_table(Apeks.TABLES.get("state_staff_positions"))
+        ),
     ).department_staff(department_id, reverse=True)
-    staff_list = [*staff_list]
+    await state.update_data(education_list=education_list)
 
+    staff_list = [*education_list]
     for i in range(1, len(staff_list), 2):
         keyboard.add(staff_list[i - 1], staff_list[i])
 
-    # для простых шагов можно не указывать название состояния, обходясь next()
     await GetSchedule.next()
     await message.answer("Теперь выберите преподавателя:", reply_markup=keyboard)
 
 
 async def schedule_staff_chosen(message: types.Message, state: FSMContext):
     saved_data = await state.get_data()
+    education_list = saved_data["education_list"]
 
-    #TODO добавить запросы и передать их в класс
-    staff_list = EducationStaff(
-        saved_data["chosen_month"],
-        saved_data["chosen_month"],
-        saved_data["chosen_year"],
-    ).department_staff(saved_data['chosen_department'], reverse=True)
-    # staff_list = {y: x for x, y in staff_list.items()}
-
-    if message.text not in staff_list:
+    if message.text not in education_list:
         await message.answer(
             "Пожалуйста, выберите преподавателя, используя клавиатуру ниже."
         )
         return
 
-    staff_id = staff_list.get(message.text)
+    staff_id = education_list.get(message.text)
+    month = saved_data["chosen_month"]
+    year = saved_data["chosen_year"]
 
-    #TODO добавить запросы и передать их в класс
-    filename = lessons_ical_exp(
-        staff_id, message.text, saved_data['chosen_month'], saved_data['chosen_year']
+    staff_lessons = ScheduleLessonsStaff(
+        staff_id,
+        month,
+        year,
+        lessons_data=await check_api_staff_lessons_response(
+            await api_get_staff_lessons(staff_id, month, year)
+        ),
+        disciplines=await get_disciplines(),
     )
 
-    if filename == 'no data':
+    staff_name = message.text
+    filename = staff_lessons.export_ical(staff_name)
+    if filename == "no data":
         await message.answer(
-            f'У {message.text} отсутствуют занятия за указанный период!',
+            f"У {message.text} отсутствуют занятия за указанный период!",
             reply_markup=types.ReplyKeyboardRemove(),
         )
     else:
-        file = open(FlaskConfig.EXPORT_FILE_DIR + filename, 'rb')
+        file = open(FlaskConfig.EXPORT_FILE_DIR + filename, "rb")
         await message.answer(
-            '{}, Ваш файл готов!'.format(message.from_user.first_name),
+            "{}, Ваш файл готов!".format(message.from_user.first_name),
             reply_markup=types.ReplyKeyboardRemove(),
         )
         await message.answer_document(document=file)
@@ -125,11 +146,10 @@ async def schedule_staff_chosen(message: types.Message, state: FSMContext):
         os.remove(FlaskConfig.EXPORT_FILE_DIR + filename)
 
     await message.answer(
-        '{}, если Вам что-то еще необходимо, '
+        "{}, если Вам что-то еще необходимо, "
         'воспользуйтесь кнопкой "Меню"!'.format(message.from_user.first_name),
         reply_markup=types.ReplyKeyboardRemove(),
     )
-
     logging.debug(
         f"Данные выбраны успешно: "
         f"год: {saved_data['chosen_year']} "
@@ -137,16 +157,13 @@ async def schedule_staff_chosen(message: types.Message, state: FSMContext):
         f"кафедра: {saved_data['chosen_department']} "
         f"преподаватель: {staff_id}",
     )
-
     await state.finish()
 
 
 def register_handlers_schedule(dp: Dispatcher):
     dp.register_message_handler(schedule_start, commands="schedule", state="*")
     dp.register_message_handler(
-        schedule_start,
-        Text(equals="Загрузить расписание", ignore_case=True),
-        state="*"
+        schedule_start, Text(equals="Загрузить расписание", ignore_case=True), state="*"
     )
     dp.register_message_handler(
         schedule_date_chosen, state=GetSchedule.waiting_for_date
