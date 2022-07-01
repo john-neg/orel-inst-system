ae88bc28e0d4a96bd7c0eaa6cc9670d056c2179b
john_neg <john_neg@mail.ru>
2022-07-01 09:48:15 +0300

    setup update

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
     name='apeks-vuz-extension',
-    version='1.0b',
+    version='1.0b0',
+    long_description=__doc__,
     packages=['apeks-vuz-extension'],
-    package_data={
-        'mypackage': ['requirements.txt'],
-    },
-    url='',
-    license='',
-    author='Evgeny Semenov',
-    author_email='',
-    description=''
+    # package_data={
+    #     'apeks-vuz-extension': ['requirements.txt'],
+    # },
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask'],
 )
