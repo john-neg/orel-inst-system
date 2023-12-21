87022c055c52b63eeedafd97b32b9374d15f5d6d
john_neg <john_neg@mail.ru>
2023-12-21 16:20:09 +0300

    staff app initial data added

--- app/db/staff_models.py
+++ app/db/staff_models.py
@@ -1,55 +1,28 @@
-from sqlalchemy import Date, Integer, ForeignKey, Text
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy.types import DateTime
+from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import Mapped, mapped_column
 
-from ..db.auth_models import Users
 from ..db.database import db
 
 
-class StaffOperations(db.Model):
-    __tablename__ = "staff_operations"
-    name: Mapped[str] = mapped_column(Text)
+class StaffStableBusyTypes(db.Model):
+    __tablename__ = 'staff_stable_busy_types'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    slug: Mapped[str] = mapped_column(String(32), unique=True)
+    name: Mapped[str] = mapped_column(String)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
 
-class StaffOperationsJournal(db.Model):
-    __tablename__ = "staff_operations_journal"
-    name: Mapped[str] = mapped_column(Text)
-    user_id: Mapped[int] = mapped_column(
-        Integer,
-        ForeignKey("users.id"),
-        nullable=False,
-    )
-    user: Mapped[Users] = relationship(
-        Users,
-        lazy="joined",
-    )
-    operation_id: Mapped[int] = mapped_column(
-        Integer,
-        ForeignKey("staff_operations.id"),
-        nullable=False,
-    )
-    time: Mapped[DateTime] = mapped_column(DateTime)
+class StaffVariousBusyTypes(db.Model):
+    __tablename__ = 'staff_various_busy_types'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    slug: Mapped[str] = mapped_column(String(32), unique=True)
+    name: Mapped[str] = mapped_column(String)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
 
 
-class StaffBusyTypes(db.Model):
-    __tablename__ = "staff_busy_types"
-    name: Mapped[str] = mapped_column(Text)
-
-
-class StaffBusyJournal(db.Model):
-    __tablename__ = "staff_busy_journal"
-    staff_id: Mapped[int] = mapped_column(
-        Integer,
-        nullable=False,
-    )
-    date: Mapped[Date] = mapped_column(Date)
-    busy_id: Mapped[int] = mapped_column(
-        Integer,
-        ForeignKey("staff_busy_types.id"),
-        nullable=False,
-    )
-    busy: Mapped[StaffBusyTypes] = relationship(
-        StaffBusyTypes,
-        lazy="joined",
-    )
-    description: Mapped[str] = mapped_column(Text)
+class StaffVariousIllnessTypes(db.Model):
+    __tablename__ = 'staff_various_illness_types'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    slug: Mapped[str] = mapped_column(String(32), unique=True)
+    name: Mapped[str] = mapped_column(String)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
